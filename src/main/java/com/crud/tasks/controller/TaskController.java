7dafba76e51efa1b1ef44711ed49271110e9d1d5
Faralek <swierczynski093@gmail.com>
2020-11-23 18:00:44 +0100

    Module 19.2 zadanie

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -6,6 +6,7 @@ import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,8 +26,8 @@ public class TaskController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test content");
+    public TaskDto getTask(@RequestParam Long taskId) {
+        return taskMapper.mapToTaskDto(dbService.findByID(taskId).get());
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
