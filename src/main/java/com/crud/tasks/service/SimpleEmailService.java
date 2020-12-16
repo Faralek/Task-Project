444be2490fc8fe352eda47aced0a57ad8a217d2b
48727 <swierczynski093@gmail.com>
2020-12-16 04:57:52 +0100

    change email settings

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -37,7 +37,6 @@ public class SimpleEmailService {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setTo(mail.getMailTo());
             messageHelper.setSubject(mail.getSubject());
-            messageHelper.setCc(mail.getToCc());
             messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
 
         };
