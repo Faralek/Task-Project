104aef4916cba75fe01232760f4bfeec4d6d4a2b
48727 <swierczynski093@gmail.com>
2020-12-16 01:12:07 +0100

    back to mysql

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -14,7 +14,7 @@ public class TasksApplication  extends SpringBootServletInitializer
         SpringApplication.run(TasksApplication.class, args);
     }
 
-//    @Override
+ //    @Override
  //   protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
  //      return application.sources(TasksApplication.class);
  //   }
